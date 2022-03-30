156075ce51ff3df96939cbb18ba88fae90a2133b
Ingvar-V <ingvarvolkov@gmail.com>
2022-03-30 21:05:44 +0300

    Update ex18.sql

--- ex18.sql
+++ ex18.sql
@@ -1,6 +1,7 @@
 /*
-Задание: 18
-Найдите производителей самых дешевых цветных принтеров. Вывести: maker, price 
+Р—Р°РґР°РЅРёРµ: 18
+РќР°Р№РґРёС‚Рµ РїСЂРѕРёР·РІРѕРґРёС‚РµР»РµР№ СЃР°РјС‹С… РґРµС€РµРІС‹С… С†РІРµС‚РЅС‹С… РїСЂРёРЅС‚РµСЂРѕРІ. Р’С‹РІРµСЃС‚Рё: maker, price 
+Solution 1
 */
 select top 1 with ties p.maker,
 r.price as price
@@ -8,5 +9,5 @@ from Printer r join
 Product p on r.model=p.model and r.color='y'
 group by r.price, p.maker
 having r.price=min(r.price)
-order by r.price
+order by r.price;
 
