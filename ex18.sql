8da2312e06a74610ae4c745cc6b805fda11635a4
Ingvar-V <ingvarvolkov@gmail.com>
2022-03-30 21:10:47 +0300

    2nd and 3rd solutions have been added

--- ex18.sql
+++ ex18.sql
@@ -3,6 +3,28 @@
 Найдите производителей самых дешевых цветных принтеров. Вывести: maker, price 
 Solution 1
 */
+with mpp as
+(SELECT MIN(price) as price 
+ FROM Printer 
+ WHERE Color ='y') 
+select distinct c.maker, p.price
+from Printer p join 
+mpp on mpp.price=p.price and p.color='y' join
+Product c ON p.model = c.model
+
+
+/* Solution 2 */
+SELECT distinct c.maker,
+       a.priceA price
+FROM (SELECT MIN(price) priceA 
+      FROM Printer 
+      WHERE Color ='y'
+     ) a INNER JOIN 
+     Printer b ON a.priceA = b.price and b.color='y' INNER JOIN 
+     Product c ON b.model = c.model
+     
+
+/* Solution 3 */
 select top 1 with ties p.maker,
 r.price as price
 from Printer r join 
