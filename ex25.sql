73b54ac5a9cd2a56c823ffdf4515799494273ef5
Ingvar-V <ingvarvolkov@gmail.com>
2022-04-02 18:38:29 +0300

    Add files via upload

---
+++ ex25.sql
@@ -0,0 +1,28 @@
+/*
+Задание: 25
+Найдите производителей принтеров, которые производят ПК с наименьшим объемом RAM и с самым быстрым процессором среди всех ПК, имеющих наименьший объем RAM. Вывести: Maker
+*/
+
+with pc_with_min_ram as (
+select model, speed 
+from PC
+where ram = (select min(ram) from PC)
+),
+
+pc_with_minram_maxspeed as (
+select model
+from pc_with_min_ram
+where speed = (select max(speed) from pc_with_min_ram)
+),
+
+printer_maker as (
+select maker 
+from Product
+where type='Printer'
+group by maker
+)
+
+select distinct maker 
+from Product
+where model in (select * from pc_with_minram_maxspeed)
+and maker in (select * from printer_maker)
