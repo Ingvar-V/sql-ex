d21d325953c801114d4bf0625bc8295ce9153646
Ingvar-V <ingvarvolkov@gmail.com>
2022-04-02 18:44:46 +0300

    Update ex25.sql

--- ex25.sql
+++ ex25.sql
@@ -15,16 +15,8 @@ pc_with_minram_maxspeed as (
 select model
 from pc_with_min_ram
 where speed = (select max(speed) from pc_with_min_ram)
-),
-
-printer_maker as (
-select maker 
-from Product
-where type='Printer'
-group by maker
 )
 
-select distinct maker 
-from Product
-where model in (select * from pc_with_minram_maxspeed)
-and maker in (select * from printer_maker)
+select maker from Product where model in (select * from pc_with_minram_maxspeed)
+intersect
+select maker from Product where type='Printer' group by maker
