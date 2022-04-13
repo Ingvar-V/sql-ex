96a574fa8e18efcb7b6c54be819f9650426960da
Ingvar-V <ingvarvolkov@gmail.com>
2022-04-13 15:32:57 +0300

    Update ex32.sql

--- ex32.sql
+++ ex32.sql
@@ -4,7 +4,18 @@
 С точностью до 2 десятичных знаков определите среднее значение mw для кораблей каждой страны, у которой есть корабли в базе данных.
 */
 
-select c.country, cast(avg(power(c.bore, 3)/2) as dec (12,2)) as mw_avg
+with ship_set as (select c.country, c.bore, s.name
 from Classes c left join
 Ships s on c.class=s.class
-group by c.country
+where s.name is not null
+union
+select distinct  c.country, c.bore, o.ship
+from outcomes o left join
+Ships s on o.ship=s.name left join
+Classes c on o.ship=c.class
+where c.country is not null)
+
+select country,
+       cast(avg(power(bore, 3)/2) as dec (12,2)) as mw_avg
+from ship_set 
+group by country
