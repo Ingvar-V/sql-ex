83f8982e6e454f0993208d180840ae64b13293e3
Ingvar-V <ingvarvolkov@gmail.com>
2022-04-15 13:59:29 +0300

    Create ex39.sql

---
+++ ex39.sql
@@ -0,0 +1,14 @@
+/*
+Задание: 39
+Найдите корабли, `сохранившиеся для будущих сражений`, т.е. выведенные из строя в одной битве (damaged), они участвовали в другой, произошедшей позже.
+*/
+
+with battle_outcome as (select o.*, b.*,
+rank() over(partition by o.ship order by b.date desc) as rnk
+from Outcomes o join
+Battles b on o.battle=b.name
+)
+select distinct ship from battle_outcome b
+where b.ship in (
+select ship from battle_outcome
+where rnk=2 and result ='damaged')
