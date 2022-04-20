d49723556cb59e23d35b0f172c27768cd514a5e3
Ingvar-V <ingvarvolkov@gmail.com>
2022-04-20 14:49:48 +0300

    Create ex47.sql

---
+++ ex47.sql
@@ -0,0 +1,28 @@
+/*
+
+*/
+
+with ships_set as (
+select country, count(name) as cnt 
+from (select c.country, s.name
+from Ships s join
+Classes c on s.class=c.class
+union
+select c.country, o.ship
+from Classes c join
+Outcomes o on c.class=o.ship) t1
+group by country),
+
+out_set as ( select country, count(name) as cnt
+from (select c.country, s.name
+      from Classes c join
+      Ships s on c.class=s.class and s.name in (select ship from Outcomes where result='sunk') 
+union
+select c.country, c.class 
+from Classes c join
+Outcomes o on c.class=o.ship and o.result='sunk') t2
+group by country)
+
+select s.country
+from ships_set s join
+out_set o on s.country = o.country and s.cnt=o.cnt; 
