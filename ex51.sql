dd5c8e00d846bcf12e750e6762ea22673bf07d62
Ingvar-V <ingvarvolkov@gmail.com>
2022-04-25 11:10:53 +0300

    Update ex51.sql

--- ex51.sql
+++ ex51.sql
@@ -3,10 +3,11 @@
 Найдите названия кораблей, имеющих наибольшее число орудий среди всех имеющихся кораблей такого же водоизмещения (учесть корабли из таблицы Outcomes).
 */
 with all_ships as(
-select s.name, numGuns, displacement from Classes c join Ships s on c.class=s.class
+select distinct s.name, numGuns, displacement from Classes c join Ships s on c.class=s.class
 union
-select o.ship as name, numGuns, displacement from Classes c join Outcomes o on c.class=o.ship),
+select distinct o.ship as name, numGuns, displacement from Classes c join Outcomes o on c.class=o.ship),
 ships_set as (select name, numGuns, displacement,
 rank() over (partition by displacement order by numGuns desc) as rnk
-from all_ships)
+from all_ships
+where numGuns is not null)
 select name from ships_set where rnk=1;
